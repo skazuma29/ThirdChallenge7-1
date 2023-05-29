38032266030d6a77fc4e7fc560ea57b667869ccc
skazuma29 <ec2-user@ip-172-31-0-223.us-west-1.compute.internal>
2023-05-29 20:27:05 +0900

    8-4課題２まで

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,29 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('posts/index')->with(['posts' => $post->getPaginationByLimit()]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
     
         //getPaginateByLimit()はPost.phpで定義したメソッドです
     }
     
+    public function show(Post $post)
+    {
+        return view("posts/show")->with(["post" => $post]);
+    //"post"はbladeファイルで使う変数。中身は＄postはid=1のpostインスタンス
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
 }
+
+
+/**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+ 
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <h1>blog name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。"></textarea>
+            </div>
+            <input type="submit" value="store"/>
+        </form>
+        <div class="footer">
+             <a href="/">戻る</a>
+        </div>
+    
+    </body>
+</html>
